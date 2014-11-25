c1c69d4771c64148e4156a3bcfb503774d471b4e
Dani Gonzalez castillo <dax01_92@MacBook-Pro-de-Dani.local>
2014-11-25 14:56:50 +0100

    Initial Commit

---
+++ EL MUNDO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EL MUNDO
+//
+//  Created by Dani Gonzalez castillo on 25/11/14.
+//  Copyright (c) 2014 Dani Gonzalez castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ EL MUNDOTests/EL_MUNDOTests.m
@@ -0,0 +1,40 @@
+//
+//  EL_MUNDOTests.m
+//  EL MUNDOTests
+//
+//  Created by Dani Gonzalez castillo on 25/11/14.
+//  Copyright (c) 2014 Dani Gonzalez castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface EL_MUNDOTests : XCTestCase
+
+@end
+
+@implementation EL_MUNDOTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
